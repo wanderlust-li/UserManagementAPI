53742f90e6676c260fbff38c90068566bd0d9b90
wanderlust-li <zhekalichm4n@icloud.com>
2023-05-21 19:45:09 +0300

    get method

---
+++ UserManagementAPI/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UserManagementAPI.Data;
+using UserManagementAPI.Models;
+
+namespace UserManagementAPI.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class UserController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public UserController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    // 
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<User>>> Get()
+    {
+        return await _context.Users.ToListAsync();
+    }
+}
