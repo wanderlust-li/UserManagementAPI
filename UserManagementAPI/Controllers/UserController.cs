6d9cb6cb1fa54dced8cb44933c1d7655b118bd61
wanderlust-li <zhekalichm4n@icloud.com>
2023-05-21 19:55:49 +0300

    update controller

--- UserManagementAPI/Controllers/UserController.cs
+++ UserManagementAPI/Controllers/UserController.cs
@@ -18,8 +18,48 @@ public class UserController : Controller
     }
     // 
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<User>>> Get()
+    public async Task<ActionResult<IEnumerable<User>>> GetAll()
     {
         return await _context.Users.ToListAsync();
+    }
+    [HttpGet("{id}")]
+    public async Task<ActionResult<User>> Get(int id)
+    {
+        var result = _context.Users.FirstOrDefault(u => u.Id == id);
+        if (result == null)
+            return NotFound();
+
+        return result;
+    }
+    [HttpPost]
+    public async Task<ActionResult<User>> Post(User user)
+    {
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(Get), new { id = user.Id }, user);
+    }
+    [HttpDelete]
+    public async Task<ActionResult<User>> Delete(int id)
+    {
+        var result = _context.Users.FirstOrDefault(u => u.Id == id);
+        if (result == null)
+            return NotFound();
+        _context.Users.Remove(result);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, User user)
+    {
+        if (id != user.Id)
+            return BadRequest();
+        _context.Entry(user).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+
     }
 }
